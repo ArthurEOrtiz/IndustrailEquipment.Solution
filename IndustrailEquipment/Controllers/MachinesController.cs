61d635d6600f90b8788454956e3c63b9cdb4917b
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-08-30 21:11:06 -0600

    debugged query functions

--- IndustrailEquipment/Controllers/MachinesController.cs
+++ IndustrailEquipment/Controllers/MachinesController.cs
@@ -22,7 +22,7 @@ namespace IndustrailEquipment.Controllers
 
         // GET: api/Machines
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Machine>>> GetMachines(string name, string serialNumber, int hours, string status)
+        public async Task<ActionResult<IEnumerable<Machine>>> Get(string name, string serialNumber, int hours, string status)
         {
             var query = _context.Machines.AsQueryable();
 
@@ -46,7 +46,7 @@ namespace IndustrailEquipment.Controllers
                 query = query.Where(entry => entry.Status == status);
             }
 
-            return await _context.Machines.ToListAsync();
+            return await query.ToListAsync();
         }
 
         // GET: api/Machines/5
