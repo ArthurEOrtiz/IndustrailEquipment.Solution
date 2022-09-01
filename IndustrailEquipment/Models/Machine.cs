ca0cfed16be3a1264013991d933a8b1dbcbd3c17
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-08-31 20:01:31 -0600

    redacting functionality attempt
    Removing some code to move along with lesson plan

--- IndustrailEquipment/Models/Machine.cs
+++ IndustrailEquipment/Models/Machine.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Configuration;
+using System.ComponentModel.DataAnnotations;
+
 namespace IndustrailEquipment.Models
 {
   public class Machine
@@ -5,7 +9,10 @@ namespace IndustrailEquipment.Models
     public int MachineId { get; set; }
     public string Name { get; set; }
     public string SerialNumber { get; set; }
-    public int Hours { get; set; }
+    public int Hours { get; set; } 
     public string Status { get; set; }
+    
   }
+
+
 }
